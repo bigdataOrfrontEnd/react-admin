30bb0fd57d164741c9e8a2142cf710e466c51231
17639638825 <562985990@qq.com>
2024-01-24 17:43:35 +0800

    左中右flex布局及数据添加

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import MainDash from "./components/main/MainDash";
+import RightSide from "./components/right/RightSide";
+import SiderBar from "./components/sider/SiderBar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="AppGlass">
+        <SiderBar />
+        <MainDash />
+        <RightSide />
+      </div>
+    </div>
+  );
 }
 
 export default App;
