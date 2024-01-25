6ce32bd070b42cb374902ddc3623f279d0883a8a
17639638825 <562985990@qq.com>
2024-01-25 14:51:21 +0800

    siderbar

--- src/components/sider/SiderBar.jsx
+++ src/components/sider/SiderBar.jsx
@@ -1,28 +1,47 @@
-import React from "react";
-import { UilBars } from "@iconscout/react-unicons";
-import Logo from '../../imgs/logo.png'
+import React, { useState } from "react";
+import { UilBars, UilSignOutAlt } from "@iconscout/react-unicons";
+import Logo from "../../imgs/logo.png";
+import { SidebarData } from "../../Data/Data";
 import { motion } from "framer-motion";
-import './siderbar.css'
+import "./siderbar.css";
 export default function SiderBar() {
-  return <div>
-    {/* <div>
+  //控制鼠标选中
+  const [select, SetSelect] = useState(0);
+  return (
+    <div>
+      {/* <div>
       <UilBars/>
     </div> */}
-    <motion.div className="sidebar">
-      {/* logo */}
-      <div className="logo">
-        <img src={Logo} alt="logo" />
-        <span>
-          Sh<span>o</span>ps
-        </span>
-
-      </div>
-      {/* mennu */}
-      <div className="menu">
-        {/* signoutIcon */}
-      </div>
-
-
-    </motion.div>
-  </div>;
+      <motion.div className="sidebar">
+        {/* logo */}
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+          <span>
+            Sh<span>o</span>ps
+          </span>
+        </div>
+        {/* mennu */}
+        <div className="menu">
+          {SidebarData.map((item, index) => {
+            return (
+              <div
+                className={select === index ? "active menuItem " : "menuItem"}
+                key={index}
+                onClick={() => {
+                  SetSelect(index);
+                }}
+              >
+                <item.icon />
+                <span>{item.heading}</span>
+              </div>
+            );
+          })}
+          {/* signoutIcon */}
+          <div className="menuItem">
+            <UilSignOutAlt />
+          </div>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
