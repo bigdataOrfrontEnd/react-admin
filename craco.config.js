89410bcaf0c7fcdcdd1866204bdd89c7777d5dff
17639638825 <562985990@qq.com>
2024-02-09 19:43:10 +0800

    tijiao

--- craco.config.js
+++ craco.config.js
@@ -9,24 +9,24 @@ module.exports = {
       // 约定
       "@": path.resolve(__dirname, "src"),
     },
-    plugins: [
-      new ModuleFederationPlugin({
-        name: "admin-react",
-        remotes: {}, //可选，表示作为 Host 时，去消费哪些 Remote；
-        shared: {
-          react: {
-            eager: true,
-            import: "react", // the "react" package will be used a provided and fallback module
-            shareKey: "react", // under this name the shared module will be placed in the share scope
-            shareScope: "default", // share scope with this name will be used
-            singleton: true, // only a single version of the shared module is allowed
-          },
-          "react-dom": {
-            eager: true,
-            singleton: true, // only a single version of the shared module is allowed
-          },
-        },
-      }),
-    ],
+    // plugins: [
+    //   new ModuleFederationPlugin({
+    //     name: "admin-react",
+    //     remotes: {}, //可选，表示作为 Host 时，去消费哪些 Remote；
+    //     shared: {
+    //       react: {
+    //         eager: true,
+    //         import: "react", // the "react" package will be used a provided and fallback module
+    //         shareKey: "react", // under this name the shared module will be placed in the share scope
+    //         shareScope: "default", // share scope with this name will be used
+    //         singleton: true, // only a single version of the shared module is allowed
+    //       },
+    //       "react-dom": {
+    //         eager: true,
+    //         singleton: true, // only a single version of the shared module is allowed
+    //       },
+    //     },
+    //   }),
+    // ],
   },
 };
--- src/components/right/RightSide.jsx
+++ src/components/right/RightSide.jsx
@@ -1,5 +1,19 @@
 import React from "react";
-
+import MarkdownIt from "markdown-it";
+import MdEditor from "react-markdown-editor-lite";
+// 导入编辑器的样式
+import "react-markdown-editor-lite/lib/index.css";
 export default function RightSide() {
-  return <div>RightSide</div>;
+  const mdParser = new MarkdownIt(/* Markdown-it options */);
+  // 完成！
+  function handleEditorChange({ html, text }) {
+    console.log("handleEditorChange", html, text);
+  }
+  return (
+    <MdEditor
+      style={{ height: "500px" }}
+      renderHTML={(text) => mdParser.render(text)}
+      onChange={handleEditorChange}
+    />
+  );
 }
