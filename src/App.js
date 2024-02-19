85906454140a25a902424a11b6c8f6434455896e
17639638825 <562985990@qq.com>
2024-02-19 14:37:51 +0800

    tijiao

--- src/App.js
+++ src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import MainDash from "./components/main/MainDash";
 import RightSide from "./components/right/RightSide";
 import SiderBar from "./components/sider/SiderBar";
+import Login from "./containers/login";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <div className="AppGlass">
-        <SiderBar />
+        {/* <SiderBar /> */}
         <Routes>
-          <Route path="/" element={<MainDash />}></Route>
+          <Route path="/" element={<Login />}></Route>
+          <Route path="/login" element={<MainDash />}></Route>
           <Route path="/expenss" element={<RightSide />}></Route>
         </Routes>
         {/* <MainDash />  
