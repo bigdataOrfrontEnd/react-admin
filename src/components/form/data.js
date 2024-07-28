67c4a7815d66e3c0d15de86349ff5eec68ea74b9
17639638825 <562985990@qq.com>
2024-07-28 08:04:24 +0800

    form

--- src/components/form/data.js
+++ src/components/form/data.js
@@ -1,8 +1,7 @@
 // data.js
 export const data = [
-    { id: 1, value: "value1", needsEdit: true },
-    { id: 2, value: "value2", needsEdit: true },
-    { id: 3, value: "value3", needsEdit: false },
-    // 其他数据项...
-    { id: 20, value: "value20", needsEdit: false }
+  { id: 1, value: "value1", needsEdit: true },
+  { id: 2, value: "value2", needsEdit: true },
+  { id: 3, value: "value3", needsEdit: false },
+  { id: 20, value: "value20", needsEdit: false },
 ];
--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -2,7 +2,7 @@
 //1.后端返回的单位净值可能是空的，这个时候如果不处理作为除数就会出错
 //2.inputNumber框保留两位小数，当后端传来的值不是两位小数如何处理
 //3.父子直接通信，尝试使用一下redux
-// 4.看懂table的某一列可编辑
+// 4.看懂table的某一列可编辑-----解决
 // 5.字典表的显示问题
 import React, { useState } from "react";
 import { data } from "./data";
@@ -18,7 +18,8 @@ const App = () => {
 export default App;
 
 const FormComponent = () => {
-  const [formData, setFormData] = useState(data);
+  // const [formData, setFormData] = useState(data);
+  const [formData] = Form.useForm();
 
   const handleChange = (id, value) => {
     setFormData((prevData) =>
@@ -28,39 +29,27 @@ const FormComponent = () => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try {
-      const response = await fetch("your-backend-api-url", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(formData),
-      });
-      if (response.ok) {
-        alert("数据提交成功");
-      } else {
-        alert("数据提交失败");
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      alert("提交过程中发生错误");
-    }
+    // try {
+    //   const response = await fetch("your-backend-api-url", {
+    //     method: "POST",
+    //     headers: {
+    //       "Content-Type": "application/json",
+    //     },
+    //     body: JSON.stringify(formData),
+    //   });
+    //   if (response.ok) {
+    //     alert("数据提交成功");
+    //   } else {
+    //     alert("数据提交失败");
+    //   }
+    // } catch (error) {
+    //   console.error("Error:", error);
+    //   alert("提交过程中发生错误");
+    // }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      {formData.map((item) => (
-        <div key={item.id}>
-          <label htmlFor={`data${item.id}`}>Data {item.id}</label>
-          <input
-            type="text"
-            id={`data${item.id}`}
-            value={item.value}
-            onChange={(e) => handleChange(item.id, e.target.value)}
-            readOnly={!item.needsEdit}
-          />
-        </div>
-      ))}
+    <form onSubmit={handleSubmit} form={formData}>
       <button type="submit">提交</button>
     </form>
   );
